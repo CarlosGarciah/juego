16717ab54b50e276c4383c3c9b3f6e9dc318d849
Carlos Garcia <carlosgarcia21pinos@gmail.com>
2016-10-20 08:53:26 +0200

    fondo

---
+++ principal.py
@@ -0,0 +1,48 @@
+
+# Módulos
+import sys, pygame
+from pygame.locals import *
+
+# Constantes
+WIDTH = 640
+HEIGHT = 480
+
+
+# Clases
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+# Funciones
+# ---------------------------------------------------------------------
+
+def load_image(filename, transparent=False):
+    image = pygame.image.load(filename)
+    image = image.convert()
+    if transparent:
+        color = image.get_at((0, 0))
+        image.set_colorkey(color, RLEACCEL)
+    return image
+
+
+# ---------------------------------------------------------------------
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Pruebas Pygame")
+
+    background_image = load_image('image.jpg')
+
+    while True:
+        for eventos in pygame.event.get():
+            if eventos.type == QUIT:
+                sys.exit(0)
+
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
+    return 0
+
+
+if __name__ == '__main__':
+    pygame.init()
+    main()
